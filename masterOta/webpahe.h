145acfa9941a59f0282c9cb8dfc95a0097cb8254
BaserTECH01 <77685916+BaserTECH01@users.noreply.github.com>
2022-05-14 18:09:09 +0300

    toggle switch eklendi

--- masterOta/webpahe.h
+++ masterOta/webpahe.h
@@ -57,7 +57,62 @@ R"=====(
      transform: translateY(4px);
     }
 
-        
+
+
+
+    .switch {
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
+}
+
+.switch input { 
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 26px;
+  width: 26px;
+  left: 4px;
+  bottom: 4px;
+  background-color: white;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+input:checked + .slider {
+  background-color: #2196F3;
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px #2196F3;
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(26px);
+  -ms-transform: translateX(26px);
+  transform: translateX(26px);
+}
+
+
+    
     
 </style>
 <!--------------------------HTML-------------------------->
@@ -80,13 +135,19 @@ R"=====(
     <th>Watts</th> 
     <th>Currents</th>
     <th>State</th>
+    <th>Mode</th>
+    <th>1.State</th>
+    <th>2.State</th>
   </tr>
   <tr style="height:100px">
     <td>Board 1</td>
     <td><span style="color:rgb(216, 3, 3)" id="board1V">0</span> W </td>
     <td><span style="color:rgb(15, 56, 245)" id="board1C">0</span> A</td>
     <th><button class="button"  id="btn"  onclick="button()" > </button></td>
-  
+    <th><p id="mode1"></p><label class="switch"> 
+  <input type="checkbox">
+  <span class="slider"></span>
+</label></td>
   </tr>
   <tr style="height:100px">
     <td>Board 2</td>
